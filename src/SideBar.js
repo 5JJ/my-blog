3e21618a5f4f6b8d1b9c61d51613fb05ce0ef79b
JeonghuiOh <jeonghui.oh@reebonz.co.kr>
2019-01-27 12:28:26 +0900

    사이드바 컴포넌트 추가 - 태그영역(미추가), 프로필영역 가짐

---
+++ src/SideBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SideBar extends Component {
+    render() {
+        return(
+            <div className="sidebar">사이드바
+            
+            
+                <div>
+                    내 프로필    
+                </div>
+            </div>
+        )    
+    }
+}
+export default SideBar;
