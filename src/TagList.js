85954c146eca2d94ceb18bb5f5b576793b5b6df9
JeonghuiOh <jeonghui.oh@reebonz.co.kr>
2019-02-23 19:09:49 +0900

    전체 태그리스트를 뿌려주는 컴포넌트 추가 - 각 태그 클릭시 액션생산자를 호출하여 표시할 컨텐츠를 변경한다

---
+++ src/TagList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class TagList extends Component {
+    id = 0;
+    state = {
+        //tags: []
+    }
+    render() {
+        let tagClassName = "tag";
+        const list = this.props.allTagList.map(
+            item => {
+                console.log(item);
+                
+                return (
+                    <span 
+                        key={item.id} 
+                        className={`tag ${item.checked ? 'checked' : 'unchecked'}`} 
+                        onClick={() => {this.props.filterContent(item.id)}}
+                        >
+                        {item.name}
+                    </span>
+                );
+            }
+        )
+
+        return(
+            <div>
+                <h4>Tag List</h4>
+                <div className="tag-list">{list}</div>
+            </div>
+        )
+    }
+}
+
+export default TagList;
